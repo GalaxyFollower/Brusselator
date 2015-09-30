3fa12f5a11fb8d4288f6bd1da1fb75087001c05a
tditada <tditada@alu.itba.edu.ar>
2015-09-30 04:30:58 -0300

    problem 1 done

--- main_test.m
+++ main_test.m
@@ -1,14 +1,14 @@
 % Tests the matrices files we already have
-% To test a different one, see test_A_generation
+% taken from http://math.nist.gov/MatrixMarket/data/NEP/mvmbwm/mvmbwm.html
+% To test a different matrix file, see test_A_generation
 function main_test()
-
-	filename1= "./matrices/bwm200.mtx";
-	filename2 = "./matrices/bwm2000.mtx";
-
-	a=2; b=5.45;
-	delt1=0.008; delt2=0.004;
-	L=0.51302;	
-	m1=100; m2=1000;
 	
+	a=2; b=5.45; delt1=0.008; delt2=0.004; L=0.51302;
+
+	filename= "./matrices/bwm200.mtx"; m=100;
+	test_A_generation (filename,a,b,delt1,delt2,L,m);
+
+	filename = "./matrices/bwm2000.mtx"; m=1000;
+	test_A_generation (filename,a,b,delt1,delt2,L,m);
 
 endfunction
