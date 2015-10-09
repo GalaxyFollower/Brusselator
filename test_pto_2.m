65f5c8e523a525a78cbbca2016845ccc0908a3b1
mariaeugenia <me.sakuda@hotmail.com>
2015-10-09 00:34:31 -0300

    punto 2 y 3 andandando (al menos para la de 200)

--- test_pto_2.m
+++ test_pto_2.m
@@ -1,10 +1,10 @@
-% Test the eigen values method.
-function T = custom_eig (m)
+%test_pto_2
+function C = test_pto_2(m)
 	a=2; b=5.45; delt1=0.008; delt2=0.004; L=0.51302;
 	A = generate_A_matrix(m, a, b, delt1, delt2, L);
 	h = 2 * m;
 	used = zeros(1, h);
-	calculated_eig = custom_eig(A, m);
+	calculated_eig = eigen_form(m, a, b, delt1, delt2, L);
 	[rightAns, EIG] = eig(A);
 	tol = 0.00001;
 	for i=1:h
@@ -17,7 +17,6 @@ function T = custom_eig (m)
 			endif
 		endfor
 		if (ansL == 0)
-			i
 			disp("test fail");
 			return;
 		endif
@@ -25,6 +24,4 @@ function T = custom_eig (m)
 	if (ansL == 1)
 		disp("test succeded");
 	endif
-endfunction
-
-
+end
