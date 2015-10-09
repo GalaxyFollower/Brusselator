3c34fba793052f59e12e91e3fb8d8798819a68c4
mariaeugenia <me.sakuda@hotmail.com>
2015-10-09 00:34:31 -0300

    punto 2 y 3 andandando (al menos para la de 200)

--- custom_eig.m
+++ custom_eig.m
@@ -3,44 +3,27 @@
 function E = custom_eig (A ,s)
 	i = size(A)(1);
 	E = zeros(1, i);
-	cota = 1e-10;
-	maxit = 100000;
-	k = 0;
+	cota = 1e-10; %si la matriz falla en algunos casos aumentar el 10 hasta que funcione, neceista más iteraciones
+
 	while i > 2 
-		[Q, R] = qr(A);
+		[Q, R] = qrhouse2(A);
 		A = R*Q;
 
 		%Recupero los autovalores de esa matriz
 		if (abs(A(i, i-1)) < (abs(A(i, i)) + abs(A(i -1, i - 1)))*cota) 
 			E(i) = A(i,i);
 			i = i - 1;
-			k = 1;
 			A = A(1:i, 1:i);
 		elseif abs(A(i-1,i-2)) < (abs(A(i-1,i-1)) + abs(A(i-2,i-2)))*cota
-			a = 1;
-			b = - (A(i-1,i-1) + A(i,i));
-			c = A(i-1, i-1)*A(i,i) - A(i-1 , i)*A(i, i-1);
-			E(i-1) = complex(-b/(2*a) + sqrt(b*b -4*a*c)/(2*a));
-			E(i) = complex(-b/(2*a) - sqrt(b*b -4*a*c)/(2*a));
+			ANS = roots([1, - (A(i-1,i-1) + A(i,i)), A(i-1, i-1)*A(i,i) - A(i-1 , i)*A(i, i-1)]);
+			E(i-1) = ANS(1);
+			E(i) = ANS(2);
 			i = i - 2;
-			%k = 1;
+			A = A(1:i, 1:i);
 		endif
-		
-		if(k == maxit)
-			A
-			a = 1;
-			b = - (A(i-1,i-1) + A(i,i));
-			c = A(i-1, i-1)*A(i,i) - A(i-1 , i)*A(i, i-1);
-			E(i-1) = complex(-b/(2*a) + sqrt(b*b -4*a*c)/(2*a));
-			E(i) = complex(-b/(2*a) - sqrt(b*b -4*a*c)/(2*a));
-			i = i - 2;
-			k = 1;
-		endif
-		%k = k + 1;
+
 	endwhile
-	a = 1;
-	b = - (A(i-1,i-1) + A(i,i));
-	c = A(i-1, i-1)*A(i,i) - A(i-1 , i)*A(i, i-1);
-	E(i-1) = complex(-b/(2*a) + sqrt(b*b -4*a*c)/(2*a));
-	E(i) = complex(-b/(2*a) - sqrt(b*b -4*a*c)/(2*a));
+	ANS = roots([1, - (A(i-1,i-1) + A(i,i)), A(i-1, i-1)*A(i,i) - A(i-1 , i)*A(i, i-1)]);
+	E(i-1) = ANS(1);
+	E(i) = ANS(2);
 endfunction
